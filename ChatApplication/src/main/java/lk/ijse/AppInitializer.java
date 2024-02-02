064a448062f7c112b72ed2f55090d163b4300fd8
SandulRusara <sandulrusara8@gmail.com>
2024-02-02 13:42:20 +0530

    Initial commit

---
+++ ChatApplication/src/main/java/lk/ijse/AppInitializer.java
@@ -0,0 +1,10 @@
+package lk.ijse;
+
+import lk.ijse.Server.ServerLauncher;
+
+public class AppInitializer {
+
+    public static void main(String[] args) {
+        ServerLauncher.main(args);
+    }
+}
